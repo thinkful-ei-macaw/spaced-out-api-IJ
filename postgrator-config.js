a77e2e661b0bf27b75b37132093a87ed107bda37
MediocreIan <idrews094@gmail.com>
2020-05-20 15:42:21 -0500

    more database setup

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
+  "ssl": !!process.env.SSL,
   "migrationDirectory": "migrations",
   "driver": "pg",
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "password": process.env.MIGRATION_DB_PASS,
 }
